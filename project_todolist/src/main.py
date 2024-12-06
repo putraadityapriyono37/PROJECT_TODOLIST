b4a5c2b4b33145a097385ebe6c4cb6999a10e8e6
askhiahasna <askhiahasna@gmail.com>
2024-12-07 00:28:25 +0700

    first page (done)

--- project_todolist/src/main.py
+++ project_todolist/src/main.py
@@ -6,6 +6,82 @@ def main(page: Page):
     FG = '#3450a1'
     PINK = '#eb06ff'
 
+    tasks = Column()
+
+    categories_card = Row(
+        scroll='auto'
+    )
+    categories = ['Business','Family','Friends']
+    for i, category in enumerate(categories):
+        categories_card.controls.append(
+            Container(
+                border_radius=20,
+                bgcolor=BG,
+                height=110,
+                width=170,
+                padding=15,
+                content=Column(
+                    controls=[
+                        Text('40 Tasks'),
+                        Text(category),
+                        Container(
+                            width=160,
+                            height=5,
+                            bgcolor='white12',
+                            border_radius=20,
+                            padding=padding.only(right=i*30),
+                            content=Container(
+                                bgcolor=PINK,
+                            ),
+
+                        )
+                    ]
+                )
+            )
+        )
+
+    first_page_contents = Container(
+        content=Column(
+            controls=[
+                Row(alignment='spaceBetween',
+                    controls=[
+                        Container(
+                            content=Icon(
+                                icons.MENU)),
+                        Row(
+                            controls=[
+                                Icon(icons.SEARCH),
+                                Icon(icons.NOTIFICATIONS_OUTLINED)
+                            ]
+                        )
+                    ]
+                ),
+                Container(height=20),
+                Text(
+                    value='What\'s up, Olivia!'
+                ),
+                Text(
+                    value='CATEGORIES'
+                ),
+                Container(
+                    padding=padding.only(top=10,bottom=20,),
+                    content=categories_card
+                ),
+                Container(height=20),
+                Text("TODAY'S TASKS"),
+                Stack(
+                    controls=[
+                        tasks,
+                        FloatingActionButton(
+                            icon = icons.ADD,on_click=lambda _: page.go('/create_task')
+
+                        )
+                    ]
+                )
+            ],
+        ),
+    )
+
     page_1 = Container()
     page_2 = Row(
         controls=[
@@ -17,10 +93,12 @@ def main(page: Page):
                 padding=padding.only(
                     top=50,left=20,
                     right=20,bottom=5
-                ),
-                content=Column(
-                    first_page_contents
-                )
+                    ),
+                    content=Column(
+                        controls=[
+                            first_page_contents
+                        ]
+                    )
             )
         ]
     )
@@ -36,9 +114,10 @@ def main(page: Page):
                 page_2
 
             ]
+
         )
+
     )
     page.add(container)
-
 
 app(target=main)
