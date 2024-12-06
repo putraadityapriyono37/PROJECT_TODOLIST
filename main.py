0a4dcd0b1c5737aa707fd9efc78bc29f579779f8
putraadityapriyono37 <putraadityap1212@gmail.com>
2024-12-06 14:02:42 +0700

    coba flet

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flet import *
+import flet as ft
 
-def main(page: Page):
-    pass
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-app(target=main,view=WEB_BROWSER)
-print('yo')
-print('p')
-print('putra')
-print ("1")
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(main)
