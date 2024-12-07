68e173147dac172342a33ce04e7778c920788d25
putraadityapriyono37 <putraadityap1212@gmail.com>
2024-12-07 12:41:19 +0700

    done

--- project_todolist/src/main.py
+++ project_todolist/src/main.py
@@ -6,9 +6,77 @@ def main(page: Page):
     FWG = '#97b4ff'
     FG = '#3450a1'
     PINK = '#eb06ff'
+    
+    circle = Stack(
+        controls=[
+            Container(
+                width=100,
+                height=100,
+                border_radius=50,
+                bgcolor='white12'
+            ),
+            Container(
+                gradient=SweepGradient(
+                    center=alignment.center,
+                    start_angle=0.0,
+                    end_angle=3,
+                    stop=[0.5, 0.5],
+                    colors=["#00000000", PINK],
+                ),
+                width=100,
+                height=100,
+                border_radius=50,
+                content=Row(
+                    alignment="center",
+                    controls=[
+                        Container(
+                            padding=padding.all(5),
+                            bgcolor=BG,
+                            width=90,
+                            height=90,
+                            border_radius=50,
+                            content=Container(
+                                bgcolor=FG,
+                                height=80,
+                                width=80,
+                                border_radius=40,
+                                content=Container(
+                                    bgcolor=FG,
+                                    height=80,
+                                    width=80,
+                                    border_radius=40,
+                                    content=CircleAvatar(
+                                        opacity=0.8,
+                                        foreground_image_src="https://images.unsplash.com/photo-1545912452-8aea7e25a3d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                                    )
+                                )
+                            )
+                        )
+                    ]
+                )
+            )
+        ]
+    )
 
     def shrink(e):
-        page_2.controls.width = 120
+        page_2.controls[0].width = 120
+        page_2.controls[0].scale = transform.Scale(
+            0.8,alignment=alignment.center_right)
+        page_2.controls[0].border_radius=border_radius.only(
+            topLeft=35,
+            topRight=0,
+            bottomLeft=35,
+            bottomRight=0
+        )
+        page_2.update()
+        
+        
+    def restore(e):
+        page_2.controls[0].width = 400
+        page_2.controls[0].border_radius = 35
+        page_2.controls[0].scale = transform.Scale(
+            1,alignment=alignment.center_right)
+        page_2.update()
 
         
     create_task_view = Container(on_click=lambda _: page.go('/'),
@@ -117,8 +185,59 @@ def main(page: Page):
         ),
     )
 
-    page_1 = Container()
-    page_2 = Row(
+    page_1 = Container(
+        width=400,
+        height=850,
+        bgcolor=BG,
+        border_radius=35,
+        padding=padding.only(left=50,top=60,right=200),
+        
+        content=Column(
+            controls=[
+                Row(
+                    controls=[
+                        Container(border_radius=25,
+                padding=padding.only(
+                    top=13,bottom=13,),
+                height=50,
+                width=50,
+                border=border.all(color='white',width=1),
+                on_click=lambda e:restore(e),
+                content=Text('<')
+                )
+                    ]
+                ),
+                Container(height=20),
+                circle,
+                Text(value='Olivia\nMitchel',size=32, weight='bold'),
+                Container(height=20),
+                Row(controls=[
+                    Icon(icons.FAVORITE_BORDER_SHARP,color='white12'),
+                    Text('Templates',size=15,weight=FontWeight.W_300,
+                         color='white',font_family='poppins')
+                ]),
+                Container(height=5),
+                Row(controls=[
+                    Icon(icons.CARD_TRAVEL,color='white12'),
+                    Text('Templates',size=15,weight=FontWeight.W_300,
+                         color='white',font_family='poppins')
+                ]),
+                Container(height=5),
+                Row(controls=[
+                    Icon(icons.CALCULATE_OUTLINED,color='white12'),
+                    Text('Templates',size=15,weight=FontWeight.W_300,
+                         color='white',font_family='poppins')
+                ]),
+                Image(src=f"/assets/1.png",
+                width=300,
+                height=200,
+                ),
+                Text('Good',color=FG,font_family='pooppins',),
+                Text('Consistency',size=22,)
+            ]
+        )
+    )
+    page_2 = Row(alignment='end',
         controls=[
             Container(
                 width=400,
@@ -181,4 +300,4 @@ def main(page: Page):
     page.on_route_change = route_change
     page.go(page.route)
 
-app(target=main)
+app(target=main,assets_dir='assets')
